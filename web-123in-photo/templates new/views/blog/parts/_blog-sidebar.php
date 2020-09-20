48a3c358df0a4c98a97805836cbafcfe5cc913c3
DzungHg <hg.dzung@gmail.com>
2020-09-21 06:23:43 +0700

    Hoàn thiện
    Blog

--- web-123in-photo/templates new/views/blog/parts/_blog-sidebar.php
+++ web-123in-photo/templates new/views/blog/parts/_blog-sidebar.php
@@ -4,11 +4,9 @@ namespace ProcessWire;
 // get latest blog post
 $latestBlogPosts = pages()->get("template=blog-posts")->children("sort=-date, limit=3");
 $titleImageLink = '';
-if ($page->template == "blog" || $page->template == "blog-category" )
-{
+if ($page->template == "blog" || $page->template == "blog-category") {
 	$titleImageLink = getImageUrlFromImages(page()->images, $page->title_image_name);
-} else if ($page->template == "blog-post")
-{
+} else if ($page->template == "blog-post") {
 	$titleImageLink = getImageUrlFromImages($page->categories->first->images, $page->categories->first->title_image_name);
 }
 ?>
@@ -20,20 +18,19 @@ if ($page->template == "blog" || $page->template == "blog-category" )
 				<h3 class="widget-title"><span>Danh mục</span></h3>
 				<ul>
 					<?php
+					$out = '';
 					foreach (pages()->find('template=blog-category') as $item) {
-					?>
-						<li><a href="<?= $item->url ?>"><?= $item->title ?></a></li>
-					<?php
+						$out .= "<li><a href='$item->url'>$item->title</a></li>";
 					}
+					echo $out;
 					?>
-
 				</ul>
 			</aside>
 		</div>
 
 		<div class="widget-item">
 			<aside class="widget tw-text-widget">
-				<img src="<?=$titleImageLink?>" />
+				<img src="<?= $titleImageLink ?>" />
 			</aside>
 		</div>
 
@@ -41,23 +38,27 @@ if ($page->template == "blog" || $page->template == "blog-category" )
 			<aside class="widget tw-post-widget">
 				<h3 class="widget-title"><span>Bài mới nhất</span></h3>
 				<ul>
-				<?php
-					foreach ($latestBlogPosts as $item)
-					{
-				 ?>
-					<li>
-						<div class="recent-thumb">
-							<a href="<?=$item->url?>"><img src="<?=$item->images->first->url?>" alt="<?=$item->images->first->description?>" /></a>
-						</div>
-						<div class="recent-content">
-							<h4>
-								<a href="<?=$item->url?>"><?=$item->title?></a>
-							</h4>
-							<span class="entry-date"><?=$item->date?></span>
-						</div>
-					</li>
 					<?php
+					$out = '';
+					$imageAltText = '';
+					$imageUrl = '';
+					foreach ($latestBlogPosts as $item) {
+						$imageAltText = $item->images->first->description;
+						$imageUrl = $item->images->first->url;
+
+						$out .= "<li>
+							<div class='recent-thumb'>
+								<a href='$item->url'><img src='$imageUrl' alt='$imageAltText' /></a>
+							</div>
+							<div class='recent-content'>
+								<h4>
+									<a href='$item->url'>$item->title</a>
+								</h4>
+								<span class='entry-date'>$item->date</span>
+							</div>
+						</li>";
 					}
+					echo $out;
 					?>
 				</ul>
 			</aside>
